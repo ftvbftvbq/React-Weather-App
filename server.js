1ae68343c44727f947d17b4191e8f1608486ef1f
ftvbftvbq <ftvbftvbq@163.com>
2016-06-27 17:21:39 +0800

    add server online part

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('https://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
+
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-  console.log('服务器启动！');
+app.listen(PORT, function() {
+  console.log('服务器启动！' + PORT);
 });
